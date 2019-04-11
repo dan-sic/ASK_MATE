5d78f93b279a88bb65e2e95a771e3c8ef25c29d7
Daniel_S <olgierd.dan@gmail.com>
2019-04-11 15:01:37 +0200

    Fix and refactor tagging functionality

--- data_manager/dm_tags.py
+++ data_manager/dm_tags.py
@@ -2,70 +2,74 @@ from connection import connection_handler
 
 
 @connection_handler
-def get_tags_of_question_by_id(cursor, id):
-    int_id = int(id)
-    cursor.execute(f"""
-                    SELECT DISTINCT *
-                        FROM question_tag INNER JOIN tag
-                            ON question_tag.tag_id = tag.id
-                    WHERE question_tag.question_id = {int_id};
-""")
-    tags = cursor.fetchall()
-    return tags
-
-
-@connection_handler
-def get_all_tags(cursor):
+def get_tags_of_question_by_id(cursor, question_id):
     cursor.execute("""
-                    SELECT DISTINCT * FROM tag 
-                    ORDER BY name ASC;
-""")
+                    SELECT DISTINCT * FROM question_tag INNER JOIN tag
+                    ON question_tag.tag_id = tag.id
+                    WHERE question_tag.question_id = %(question_id)s;
+                    """,
+                   {'question_id': question_id})
     tags = cursor.fetchall()
     return tags
 
 
 @connection_handler
-def add_tag_to_question(cursor, id, tag_id):
-    cursor.execute(f"""
-                    INSERT INTO question_tag (question_id, tag_id)
-                    VALUES ({id}, {tag_id})
-""")
+def add_tags_to_question(cursor, form_data, question_id):
+    # Sample form_data:
+    # ImmutableMultiDict([('3', 'on'), ('new_tag', 'ooo')])
+    for tag_id, selected in form_data.items():
+        # if if iteration is on new_tag and there is no new tag - do nothing
+        if not selected:
+            continue
+        # if iteration is on new_tag and new tak is not empty
+        if tag_id == 'new_tag' and selected:
+            new_tag = get_tag_data_by_tag_name(selected)
+            tag_id = new_tag['id']
+        cursor.execute("""
+                        INSERT INTO question_tag (question_id, tag_id)
+                        VALUES (%(question_id)s, %(tag_id)s)
+                        """,
+                       {'question_id': question_id, 'tag_id': tag_id})
 
 
 @connection_handler
-def get_tag_id_by_tag_name(cursor, tag_name):
-    if tag_name['tag'] != 'other':
-        cursor.execute(f"""
-                        SELECT * FROM tag
-                        WHERE name = '{tag_name['tag']}'
-    """)
-    else:
-        cursor.execute(f"""
-                            INSERT INTO tag (name)
-                            VALUES ('{tag_name['other']}');
-                            SELECT * FROM tag
-                            WHERE name = '{tag_name['other']}'                   
-        """)
-    result = cursor.fetchall()
-    return result
+def get_tag_data_by_tag_name(cursor, tag_name):
+    cursor.execute("""
+                    SELECT * FROM tag
+                    WHERE name = %(name)s                  
+                    """, {'name': tag_name})
+    tag = cursor.fetchone()
+    return tag
 
 
 @connection_handler
-def get_new_tags(cursor, id):
-    cursor.execute(f"""
+def get_tags(cursor, question_id):
+    cursor.execute("""
                     SELECT tag.id, tag.name FROM tag
                     WHERE tag.id NOT IN (
                         SELECT tag_id FROM question_tag
-                        WHERE question_id = {id}
+                        WHERE question_id = %(question_id)s
                         );
-""")
+                    """,
+                   {'question_id': question_id})
     tags = cursor.fetchall()
     return tags
+
+
+@connection_handler
+def add_new_tag(cursor, new_tag):
+    # todo > now if we try to add tag which already exists - there is psycopg2.IntegrityError > handle it
+    cursor.execute("""
+                    INSERT INTO tag (name)
+                    VALUES (%(tag_name)s);
+                    """,
+                   {'tag_name': new_tag})
 
 
 @connection_handler
 def delete_tag_from_question(cursor, question_id, tag_id):
-    cursor.execute(f"""
+    cursor.execute("""
                     DELETE FROM question_tag
-                    WHERE question_id = {question_id} AND tag_id = {tag_id};
-""")
+                    WHERE question_id = %(question_id)s AND tag_id = %(tag_id)s;
+                    """,
+                   {'question_id': question_id, 'tag_id': tag_id})
--- server_python/sv_tags.py
+++ server_python/sv_tags.py
@@ -3,15 +3,18 @@ from data_manager import dm_tags
 from flask import request, redirect, render_template
 
 
-@app.route('/question/<id>/new-tag', methods=['GET', 'POST'])
-def route_add_tag(id):
-    action = '/question/' + id + '/new-tag'
-    options = dm_tags.get_new_tags(id)
+@app.route('/question/new-tag', methods=['GET', 'POST'])
+def route_add_tag():
+    question_id = request.args.get('question_id')
+    options = dm_tags.get_tags(question_id)
     if request.method == 'POST':
-        tag_id = dm_tags.get_tag_id_by_tag_name(request.form)
-        dm_tags.add_tag_to_question(id, tag_id[0]['id'])
-        return redirect(f'question_detail/{id}')
-    return render_template('tag.html', options=options, action=action)
+        print(request.form)
+        if request.form.get('new_tag'):
+            new_tag = request.form.get('new_tag')
+            dm_tags.add_new_tag(new_tag)
+        dm_tags.add_tags_to_question(request.form, question_id)
+        return redirect(f'/question_detail/{question_id}')
+    return render_template('tag.html', options=options, question_id=question_id)
 
 
 @app.route('/question_detail/<question_id>/tag/<tag_id>/delete', methods=['GET'])
