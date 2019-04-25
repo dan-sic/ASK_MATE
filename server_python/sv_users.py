5f2ee6bc29ffb33e04e370af0a8378e01de50bc1
Daniel_S <olgierd.dan@gmail.com>
2019-04-25 11:04:29 +0200

    Add user page route

--- server_python/sv_users.py
+++ server_python/sv_users.py
@@ -7,17 +7,20 @@ from flask import request, redirect, render_template, jsonify
 @app.route('/users_list', methods=['GET', 'POST'])
 def route_list_users():
     users = dm_users.get_users()
-    print(users)
     return render_template('users.html', users=users)
 
 
 @app.route('/sort_users', methods=['GET', 'POST'])
 def route_sort_users():
     feature_to_order_by = request.args.get('order_by', default='username', type=str)
-    # todo > everywhere we receive user input - evein in URL - make checks if parameters exist
-    # if feature_to_order_by not in ['title', 'message']:
-    #     return 'Error: wrong parameter'
     order_direction = request.args.get('order_direction', default='asc', type=str)
     users = dm_users.get_users_sorted(feature_to_order_by, order_direction)
     return render_template('users.html',
                            users=users)
+
+
+@app.route('/user_page', methods=['GET', 'POST'])
+def user_page():
+    user_id = request.args.get('user_id')
+    user_data = dm_users.get_user(user_id)
+    return render_template('user_page.html', user=user_data)
